d98ac43dbf02d7dd115d6a8d28650ed2b0d97f65
prajjwolbhattarai <84637244+prajjwolbhattarai@users.noreply.github.com>
2025-04-02 14:31:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/',  // ← important
+  base: '/prajjwol-career-adventure/', // Replace with your repo name
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
